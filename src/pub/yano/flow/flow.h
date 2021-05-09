6d6663a24f71b404fec687c1a35e0e1f454a2826
Adrian Visarra <adrianv@spotify.com>
2021-05-09 13:56:31 +0200

    Add tests

--- src/pub/yano/flow/flow.h
+++ src/pub/yano/flow/flow.h
@@ -9,22 +9,22 @@
 
 namespace yano {
 
-class FlowOption {
+class Flow final {
  public:
-  FlowOption() = default;
-  FlowOption(std::error_code ec) : _ec(ec) {}
-  std::error_code ec() const { return _ec; }
+  Flow() = default;
+  Flow(std::error_code ec) : _ec(ec) {}
+  inline std::error_code ec() const { return _ec; }
 
-  template <typename HappyPath, typename = std::enable_if_t<std::is_convertible_v<
-                                    std::invoke_result_t<HappyPath>, FlowOption>>>
-  FlowOption operator|(HappyPath hpath) const {
+  template <typename HappyPath, typename = std::enable_if_t<
+                                    std::is_convertible_v<std::invoke_result_t<HappyPath>, Flow>>>
+  Flow operator|(HappyPath hpath) const {
     if (_ec) {
       return _ec;
     }
     return hpath();
   }
 
-  std::error_code operator|(std::error_code) const { return _ec; }
+  inline std::error_code operator|(std::error_code) const { return _ec; }
 
  private:
   std::error_code _ec;
--- test/flow_test.cpp
+++ test/flow_test.cpp
@@ -2,4 +2,70 @@
 
 #include "yano/flow/flow.h"
 
-TEST(flow_test, dummy) { EXPECT_TRUE(true); }
+#include <stdexcept>
+#include <string>
+
+namespace {
+
+class A {
+ public:
+  std::error_code ok1() {
+    _flow += "ok1";
+    return {};
+  }
+  std::error_code ok2() {
+    _flow += "ok2";
+    return {};
+  }
+  std::error_code error1() {
+    _flow += "error1";
+    return std::make_error_code(std::errc::io_error);
+  }
+  std::error_code error2() {
+    _flow += "error2";
+    try {
+      throw std::exception();
+    } catch (...) {
+      return std::make_error_code(std::errc::invalid_argument);
+    }
+    return std::make_error_code(std::errc::io_error);
+  }
+
+  std::string check() const { return _flow; }
+
+ private:
+  std::string _flow;
+};
+
+}  // namespace
+
+TEST(flow_test, no_error) {
+  A a;
+  const auto ec = yano::Flow(a.ok1()) | [&] { return a.ok2(); } | std::error_code();
+  EXPECT_EQ(a.check(), "ok1ok2");
+  EXPECT_EQ(ec, std::error_code());
+}
+
+TEST(flow_test, error_at_the_beginning) {
+  A a;
+  const auto ec = yano::Flow(a.error1()) | [&] { return a.ok1(); } | [&] { return a.ok2(); } |
+                  std::error_code();
+  EXPECT_EQ(a.check(), "error1");
+  EXPECT_EQ(ec, std::errc::io_error);
+}
+
+TEST(flow_test, error_in_the_middle) {
+  A a;
+  const auto ec = yano::Flow(a.ok1()) | [&] { return a.error2(); } | [&] { return a.ok2(); } |
+                  std::error_code();
+  EXPECT_EQ(ec, std::errc::invalid_argument);
+  EXPECT_EQ(a.check(), "ok1error2");
+}
+
+TEST(flow_test, error_at_the_end) {
+  A a;
+  const auto ec = yano::Flow(a.ok1()) | [&] { return a.ok2(); } | [&] { return a.error2(); } |
+                  std::error_code();
+  EXPECT_EQ(a.check(), "ok1ok2error2");
+  EXPECT_EQ(ec, std::errc::invalid_argument);
+}
